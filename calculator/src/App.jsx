be42905de328de0f2b11aac4643b18af84bd331d
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-09-16 09:53:57 +0200

    I tried not hard and got so far

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,13 +1,46 @@
-import { Calculator } from "./components/Calculator/Calc"
+import { useState } from "react";
+import styles from './components/Calculator/Calc.module.scss';
 
-function App() {
- 
-
-  return (
-    <>
-     
-    </>
-  )
-}
-
-export default App
+export default function App() {
+const [currentState, setCurrentState] = useState(0);
+return (
+<div>
+  <button
+    className={styles.Button1}
+    name="1"
+    value={1}
+    onClick={() => setCurrentState(1)} 
+  >
+    1
+  </button>
+  <button
+    className={styles.Button2}
+    name="2"
+    value={2}
+    onClick={() => setCurrentState(2)}
+  >
+    2
+  </button>
+  <button className={styles.Plus} name="+" onClick={() => 
+  setCurrentState("+")}>
+    +
+  </button>
+  <button
+    className={styles.Equals}
+    name="="
+    onClick={() => setCurrentState()}
+  >
+    =
+  </button>
+  <button
+    className={styles.Reset}
+    name="reset"
+    value={0}
+    onClick={() => setCurrentState(0)} //when clicked it will set to 0
+  >
+    0
+  </button>
+  <h2>{currentState}</h2>
+  </div>
+  );
+  }
